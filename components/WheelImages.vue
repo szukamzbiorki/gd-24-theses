75877f8cd9d4c007980b8b9bbeb5214cfe8e29ca
Bartek Pierściński <bartoszjpierscinski@gmail.com>
2024-03-13 11:50:25 +0100

    add scroll

--- components/WheelImages.vue
+++ components/WheelImages.vue
@@ -42,6 +42,61 @@
 
 	defineExpose({ changeSlide })
 
+	const WheelControls = (slider) => {
+		let touchTimeout
+		let position
+		let wheelActive
+
+		function dispatch(e, name) {
+			position.x -= e.deltaX
+			position.y -= e.deltaY
+			slider.container.dispatchEvent(
+				new CustomEvent(name, {
+					detail: {
+						x: position.x,
+						y: position.y,
+					},
+				})
+			)
+		}
+
+		function wheelStart(e) {
+			position = {
+				x: e.pageX,
+				y: e.pageY,
+			}
+			dispatch(e, 'ksDragStart')
+		}
+
+		function wheel(e) {
+			dispatch(e, 'ksDrag')
+		}
+
+		function wheelEnd(e) {
+			dispatch(e, 'ksDragEnd')
+		}
+
+		function eventWheel(e) {
+			e.preventDefault()
+			if (!wheelActive) {
+				wheelStart(e)
+				wheelActive = true
+			}
+			wheel(e)
+			clearTimeout(touchTimeout)
+			touchTimeout = setTimeout(() => {
+				wheelActive = false
+				wheelEnd(e)
+			}, 50)
+		}
+
+		slider.on('created', () => {
+			slider.container.addEventListener('wheel', eventWheel, {
+				passive: false,
+			})
+		})
+	}
+
 	const options = {
 		loop: false,
 		mode: 'free-snap',
@@ -66,7 +121,7 @@
 	}
 
 	onMounted(() => {
-		slider.value = new KeenSlider(container.value, options)
+		slider.value = new KeenSlider(container.value, options, [WheelControls])
 	})
 </script>
 
--- components/WheelTitles.vue
+++ components/WheelTitles.vue
@@ -63,6 +63,61 @@
 	const emit = defineEmits(['slideChanged'])
 	defineExpose({ changeSlide })
 
+	const WheelControls = (slider) => {
+		let touchTimeout
+		let position
+		let wheelActive
+
+		function dispatch(e, name) {
+			position.x -= e.deltaX
+			position.y -= e.deltaY
+			slider.container.dispatchEvent(
+				new CustomEvent(name, {
+					detail: {
+						x: position.x,
+						y: position.y,
+					},
+				})
+			)
+		}
+
+		function wheelStart(e) {
+			position = {
+				x: e.pageX,
+				y: e.pageY,
+			}
+			dispatch(e, 'ksDragStart')
+		}
+
+		function wheel(e) {
+			dispatch(e, 'ksDrag')
+		}
+
+		function wheelEnd(e) {
+			dispatch(e, 'ksDragEnd')
+		}
+
+		function eventWheel(e) {
+			e.preventDefault()
+			if (!wheelActive) {
+				wheelStart(e)
+				wheelActive = true
+			}
+			wheel(e)
+			clearTimeout(touchTimeout)
+			touchTimeout = setTimeout(() => {
+				wheelActive = false
+				wheelEnd(e)
+			}, 50)
+		}
+
+		slider.on('created', () => {
+			slider.container.addEventListener('wheel', eventWheel, {
+				passive: false,
+			})
+		})
+	}
+
 	const options = {
 		loop: false,
 		mode: 'snap',
@@ -89,7 +144,7 @@
 	}
 
 	onMounted(() => {
-		slider.value = new KeenSlider(container.value, options)
+		slider.value = new KeenSlider(container.value, options, [WheelControls])
 	})
 </script>
 
