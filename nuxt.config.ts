bc6430c52551feb3e99e6eadc84405bfc5f2551f
Bartek Pierściński <bartoszjpierscinski@gmail.com>
2025-06-24 22:41:43 +0200

    push

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,27 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	devtools: { enabled: false },
-	css: ['~/assets/css/index.css'],
-	app: {
-		baseURL: '/gd-24-theses/',
-		pageTransition: {
-			name: 'fade',
-			mode: 'out-in',
-		},
-		layoutTransition: {
-			name: 'fade',
-			mode: 'out-in',
-		},
-		head: {
-			link: [
-				{
-					rel: 'icon',
-					type: 'image/x-icon',
-					href: '/gd-24-theses/favicon.ico',
-				},
-			],
-		},
-	},
-	modules: ['@vueuse/nuxt', '@nuxt/image', '@nuxt/content', 'nuxt-marquee'],
-	ssr: true,
-})
+  compatibilityDate: '2025-06-24',
+  devtools: { enabled: false },
+  css: ['~/assets/css/index.css'],
+  app: {
+    baseURL: '/gd-24-theses/',
+    pageTransition: {
+      name: 'fade',
+      mode: 'out-in',
+    },
+    layoutTransition: {
+      name: 'fade',
+      mode: 'out-in',
+    },
+    head: {
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/gd-24-theses/favicon.ico',
+        },
+      ],
+    },
+  },
+  modules: ['@vueuse/nuxt', '@nuxt/image', '@nuxt/content', 'nuxt-marquee'],
+  ssr: true,
+});
